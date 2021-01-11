8552ffff85b088fbe2bbd15dd68190301a306e06
KeithBlackwelder <nolimits1120@gmail.com>
2021-01-11 06:02:56 -0500

    set up react js files\n and home starter

---
+++ resources/js/components/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+
+if (document.getElementById("root")) {
+    ReactDOM.render(<Home />, document.getElementById("root"));
+}
---
+++ resources/js/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <h1 className="text-center">Welcome To Coding Blind</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
